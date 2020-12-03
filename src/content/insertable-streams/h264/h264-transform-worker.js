870de5f78fdc9b33b3a3f83e8023468666357743
Youenn Fablet <youennf@gmail.com>
2020-12-03 10:37:01 +0100

    add escaping

--- src/content/insertable-streams/h264/h264-transform-worker.js
+++ src/content/insertable-streams/h264/h264-transform-worker.js
@@ -1,19 +1,26 @@
 let currentCryptoKey = "dd";
 let currentKeyIdentifier = 0;
 
-function dump(encodedFrame, direction, max = 16) {
-    if (encodedFrame.type === "key")
-        max = 48;
-    const data = new Uint8Array(encodedFrame.data);
+function dumpData(buffer, max)
+{
+    const data = new Uint8Array(buffer);
     let bytes = '';
     for (let j = 0; j < data.length && j < max; j++)
         bytes += (data[j] < 16 ? '0' : '') + data[j].toString(16) + ' ';
+    console.log(bytes.trim());
+}
 
-    console.log(performance.now().toFixed(2), direction, bytes.trim(),
+function dump(encodedFrame, direction, max = 16) {
+    if (encodedFrame.type === "key")
+        max = 48;
+    const data = new Uint8Array(encodedFrame.data, max);
+    console.log(performance.now().toFixed(2), direction,
         'len=' + encodedFrame.data.byteLength,
         'type=' + (encodedFrame.type || 'audio'),
         'ts=' + encodedFrame.timestamp,
     );
+
+    dumpData(encodedFrame.data);
 }
 
 function hasFrameSPSPPS(encodedFrame)
@@ -22,6 +29,112 @@ function hasFrameSPSPPS(encodedFrame)
       return false;
     const view = new DataView(encodedFrame.data);
     return view.getInt8(0) === 0 && view.getInt8(1) === 0 && view.getInt8(2) === 0 && view.getInt8(3) === 1 && ((view.getInt8(4) & 0x1F) === 7);
+}
+
+function countRbspEscapingOccurences(data, offset)
+{
+    let count = 0;
+    const view = new DataView(data);
+    let numConsecutiveZeros = 0;
+    for (let i = offset; i < data.byteLength; ++i) {
+        const byte = view.getUint8(i);
+        if (byte <= 3 && numConsecutiveZeros >= 2) {
+            count++;
+            numConsecutiveZeros = 0;
+        }
+        if (byte === 0)
+            ++numConsecutiveZeros;
+        else
+            numConsecutiveZeros = 0;
+    }
+    return count;
+}
+
+function countRbspUnescapingOccurences(data, offset)
+{
+    let count = 0;
+    const view = new DataView(data);
+    if (data.byteLength < 3)
+        return 0;
+    for (let i = offset; i < data.byteLength - 3; ++i) {
+        const byte1 = view.getUint8(i);
+        const byte2 = view.getUint8(i + 1);
+        const byte3 = view.getUint8(i + 2);
+        if (byte1 === 0 && byte2 === 0 && byte3 === 3) {
+            i += 2;
+            count++;
+        }
+    }
+    return count;
+}
+
+function toRbsp(data, offset)
+{
+    const count = countRbspEscapingOccurences(data, offset);
+    if (!count)
+        return data;
+
+console.log("do escaping");
+
+    const view = new DataView(data);
+    const newData = new ArrayBuffer(data.byteLength + count);
+    const newView = new DataView(newData);
+
+    for (let i = 0; i < offset; ++i)
+        newView.setUint8(i, view.getUint8(i));
+
+    let counter = offset;
+    let numConsecutiveZeros = 0;
+    for (let i = offset; i < data.byteLength; ++i) {
+        const byte = view.getUint8(i);
+        if (byte <= 3 && numConsecutiveZeros >= 2) {
+            numConsecutiveZeros = 0;
+console.log("inserting escape at " + counter);
+            newView.setUint8(counter++, 3);
+        }
+        newView.setUint8(counter++, byte);
+        if (byte === 0)
+            ++numConsecutiveZeros;
+        else
+            numConsecutiveZeros = 0;
+    }
+
+    return newData;
+}
+
+function fromRbsp(data, offset)
+{
+    const count = countRbspUnescapingOccurences(data, offset);
+    if (!count)
+        return data;
+
+console.log("do unescaping");
+
+    const view = new DataView(data);
+    const newData = new ArrayBuffer(data.byteLength - count);
+    const newView = new DataView(newData);
+
+    let counter = 0;
+    for (let i = 0; i < offset; ++i)
+        newView.setUint8(counter++, view.getUint8(i));
+
+    for (let i = offset; i < data.byteLength - 3; ++i) {
+        const byte1 = view.getUint8(i);
+        const byte2 = view.getUint8(i + 1);
+        const byte3 = view.getUint8(i + 2);
+        if (byte1 === 0 && byte2 === 0 && byte3 === 3) {
+            newView.setUint8(counter++, byte1);
+            newView.setUint8(counter++, byte2);
+            // Skip next byte which is delimiter.
+            i += 2;
+console.log("removing escape at " + i);
+        } else
+            newView.setUint8(counter++, byte1);
+    }
+    for (let i = data.byteLength - 3; i < data.byteLength; ++i)
+        newView.setUint8(counter++, view.getUint8(i));
+
+    return newData;
 }
 
 function encryptFrameWithPrefix(encodedFrame, prefix)
@@ -49,14 +162,15 @@ function encryptFrameWithPrefix(encodedFrame, prefix)
     // Append checksum
     newView.setUint32(counter, 0xDEADBEEF);
 
-    encodedFrame.data = newData;
+    encodedFrame.data = toRbsp(newData, prefix.length + 1);
 }
 
 function decryptFrameSkipPrefix(encodedFrame, prefixLength)
 {
-    let length = encodedFrame.data.byteLength - prefixLength - 1 - 4; // 1 is for the key ID and last 4 is for the checksum.
+    const data = fromRbsp(encodedFrame.data, prefixLength + 1);
+    let length = data.byteLength - prefixLength - 1 - 4; // 1 is for the key ID and last 4 is for the checksum.
 
-    const view = new DataView(encodedFrame.data);
+    const view = new DataView(data);
 
     const newData = new ArrayBuffer(length);
     const newView = new DataView(newData);
@@ -65,14 +179,15 @@ function decryptFrameSkipPrefix(encodedFrame, prefixLength)
     // Skip prefix and key identifier.
     const start = prefixLength + 1;
     // Skip checksum
-    const end = encodedFrame.data.byteLength - 4;
+    const end = data.byteLength - 4;
+
     // xor content
     for (let i = start; i < end; ++i) {
         const keyByte = currentCryptoKey.charCodeAt(i % currentCryptoKey.length);
         newView.setInt8(counter++, view.getInt8(i) ^ keyByte);
     }
 
-    const checksum = encodedFrame.data.byteLength > 4 ? view.getUint32(encodedFrame.data.byteLength - 4) : false;
+    const checksum = data.byteLength > 4 ? view.getUint32(data.byteLength - 4) : false;
     if (checksum !== 0xDEADBEEF) {
         console.log('Corrupted frame received, checksum ' + checksum.toString(16));
         return;
@@ -97,7 +212,7 @@ function encryptFrame(encodedFrame)
        encryptFrameWithPrefix(encodedFrame, prefixKeyFrameWithSPSPPS);
         return;
     }
-    encryptFrameWithPrefix(encodedFrame, prefixKeyFrameWithSPSPPS);
+    encryptFrameWithPrefix(encodedFrame, prefixKeyFrameWithoutSPSPPS);
 }
 
 function decryptFrame(encodedFrame)
